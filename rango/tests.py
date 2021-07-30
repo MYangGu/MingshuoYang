e454f1f71029c9365695680d22b1465843f1e104
YMS <2572280y@student.gla.ac.uk>
2021-07-30 11:39:07 +0100

    I will downgrade Django. Please check the new share in the future Commit

--- rango/tests.py
+++ rango/tests.py
@@ -2,9 +2,7 @@ from django.test import TestCase
 
 class CategoryMethodTests(TestCase):
  def test_ensure_views_are_positive(self):
-"""
-Ensures the number of views received for a Category are positive or zero.
-"""
+
 category = Category(name='test', views=-1, likes=0)
 category.save()
 self.assertEqual((category.views >= 0), True)
